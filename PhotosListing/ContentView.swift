46d1d59af686ef8e990d304296e443662987cbe7
imacapella <gurkankaradas42@gmail.com>
2025-02-26 17:25:02 +0300

    Initial Commit

---
+++ PhotosListing/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PhotosListing
+//
+//  Created by Gürkan Karadaş on 26.02.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PhotosListing/PhotosListingApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotosListingApp.swift
+//  PhotosListing
+//
+//  Created by Gürkan Karadaş on 26.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct PhotosListingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
